797adec06a702e7970863a363acc6c50139446c6
ibrahimkapici <ibrhm.kapici@gmail.com>
2022-07-16 07:45:48 +0300

    second commit

---
+++ src/main/java/com/getused/second/DemoGetusedApplication.java
@@ -0,0 +1,13 @@
+package com.getused.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGetusedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGetusedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getused/second/DemoGetusedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getused.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGetusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
